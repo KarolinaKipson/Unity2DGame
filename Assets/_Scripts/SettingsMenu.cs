a3da01582a17d6e91d2a5b43058a751e502c9b41
Karolina Kipson <karolinakipson@gmail.com>
2019-10-13 00:48:53 +0200

    All bugs fixed

--- Assets/_Scripts/SettingsMenu.cs
+++ Assets/_Scripts/SettingsMenu.cs
@@ -2,16 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
     public AudioMixer volumeMixer;
+    public Slider volumeSlider;
 
     public void SetVolume(float volume)
     {
-        // Debug.Log(volume);
+        Debug.Log(volume);
+
         volumeMixer.SetFloat("volume", volume);
         PlayerPrefs.SetFloat("volume", volume);
+
+        volumeSlider.value = PlayerPrefs.GetFloat("volume");
     }
 
     public void SetQuality(int qualityIndex)
